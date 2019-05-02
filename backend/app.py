1f9c321101a01c20689aee56705a1b4a3a890f2d
Brendan <brendanbath@gmail.com>
2019-05-02 14:36:53 +0200

    added tests for verify

--- backend/app.py
+++ backend/app.py
@@ -17,24 +17,30 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
-    try:
+    if not os.path.isdir(app.instance_path):
         os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    # a simple page that says hello
     @app.route('/api/verify', methods=['POST'])
     def verify():
+        """A method to verify a user and supply them with a token to access the service
+
+        :return: if the request does not contain a body a response is returned with status 400 and text "Request body
+        missing". If the body is missing either the id or secret field a response is returned with status 401 and text
+        "Invalid id or secret". If the request is valid a response containing a json object containing a success flag
+        and token is returned with status 200.
+        """
         body = request.get_json()
+        # ensure the body exists
         if body:
             body_id = body.get("id")
             body_secret = body.get("secret")
 
+            # ensure the id and secret exist
             if body_id and body_secret:
                 json_val = json.dumps({"success": True, "token": str(uuid.uuid4())})
                 return Response(json_val, status=200, mimetype='application/json')
             return Response("Invalid id or secret", status=401, mimetype='application/text')
         else:
-            return Response("invalid request body missing", status=400, mimetype='application/text')
+            return Response("Request body missing", status=400, mimetype='application/text')
 
     return app
