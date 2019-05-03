af0e8816e3c1b36c379c956f837d56c0b80d648e
Jessica <jessicadasilva1997@gmail.com>
2019-05-03 10:27:14 +0200

    Login screen added

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card> 
+                  <v-layout justify-center row>
+                     <v-flex xs4 md2>
+                        <v-layout justify-center>
+                          <v-card-title primary-title>
+                            <div>
+                               <h3 class="headline mb-0">Amazon Dash</h3>
+                            </div>
+                          </v-card-title>
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-card-text>
+                        <v-form @submit.prevent="login()" v-model="valid">
+                         
+                         <v-text-field class="username"
+                           label="Username"
+                           prepend-icon="person"
+                           required
+                           v-model="username"
+                           :rules="[(v => !!v || 'Username is required')]"
+                         ></v-text-field>
+                         <v-text-field class="password"
+                           label="Password"
+                           :type="passwordShow ? 'text' : 'password'"
+                           prepend-icon="lock"
+                           :append-icon="passwordShow ? 'visibility_off' : 'visibility'"
+                           @click:append="passwordShow = !passwordShow"
+                           required
+                           v-model="password"
+                           :rules="[(v => !!v || 'Password is required')]"
+                        ></v-text-field>
+                          <v-btn type="submit">Login</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-flex>
+              </v-layout>
+            </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Login extends Vue {
+    private passwordShow: boolean = false
+    private valid: boolean = false
+    private username: string = ''
+    private password: string = ''
+    private login() {
+        const payload = { email: this.username, password: this.password }
+        fetch('http://192.168.43.226:5000/api/login', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        }).then(response => response.json()).then(res => this.$store.commit('setToken', res.token))
+        .then(() => this.$router.push('/'))
+    }
+}
+</script>
