af31d1c922368117d97f3045560dc56d896874cc
Jessica <jessicadasilva1997@gmail.com>
2019-05-24 12:17:18 +0200

    fixed things

--- frontend/src/views/Services.vue
+++ frontend/src/views/Services.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container grid-list-md>
     <v-layout row>
-      <v-flex xs6 v-for="service in services" :key="service.id">
-        <ServiceCard :title="service.title"></ServiceCard>
+      <v-flex xs6 v-for="service in $store.getters.instances" :key="service.id">
+        <ServiceCard :title="service.KeyName"></ServiceCard>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,7 +10,6 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import ServiceCard from '@/components/ServiceCard.vue'
-import config from '@/config'
 
 @Component({
   components: { ServiceCard },
@@ -18,15 +17,7 @@ import config from '@/config'
 export default class Services extends Vue {
   private services: any[] = []
   private mounted() {
-    fetch(config.apiUrl + '/ec2_instances', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ token: this.$store.getters.token }),
-    }).then(response => response.json())
-      .then(res => this.services = res)
+    this.$store.dispatch('fetchInstances')
   }
 }
 </script>
